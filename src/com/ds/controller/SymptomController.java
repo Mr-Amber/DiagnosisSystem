8b38039ea2b7d44ee5f2adb3d570a051e1e55445
raledong <raledong@gmail.com>
2017-12-17 20:48:44 +0800

    添加symptomController需要的接口

--- src/com/ds/controller/SymptomController.java
+++ src/com/ds/controller/SymptomController.java
@@ -4,4 +4,52 @@ import com.ds.service.SymptomService;
 import com.ds.service.impl.SymptomServiceImpl;
 
 public class SymptomController {
+
+    private SymptomService symptomService = SymptomServiceImpl.getInstance();
+
+    public void index(){
+
+    }
+
+    /**
+     * 删除大类
+     */
+    public void deleteSymptom(){
+
+    }
+
+    /**
+     * 删除小类
+     */
+    public void deleteSubSymptom(){
+
+    }
+
+    /**
+     * 添加大类
+     */
+    public void addSymptom(){
+
+    }
+
+    /**
+     * 添加小类
+     */
+    public void addSubSymptom(){
+
+    }
+
+    /**
+     * 更新大类
+     */
+    public void updateSymptom(){
+
+    }
+
+    /**
+     * 更新子大类
+     */
+    public void updateSubSymptom(){
+
+    }
 }
